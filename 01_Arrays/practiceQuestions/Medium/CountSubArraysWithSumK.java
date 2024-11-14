db9b62236670533679024ecc21271e5afc694355
MonishGiri <monishgiri46@gmail.com>
2024-11-14 14:15:16 +0530

    Count Sub Arrays with Sum k

--- 01_Arrays/practiceQuestions/Medium/CountSubArraysWithSumK.java
+++ 01_Arrays/practiceQuestions/Medium/CountSubArraysWithSumK.java
@@ -1,25 +1,30 @@
 package Medium;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class CountSubArraysWithSumK {
     public static int subArraySum(int[] arr, int k){
         int n = arr.length;
         if(n == 0) return 0;
         int count = 0;
-        int sum = 0;
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        int preSum = 0;
+
+        map.put(0,1);
+
         for(int i=0; i<n; i++){
-            // int difference = k - sum;
-            if(arr[i] == k) {
-                System.out.println("hello");
-                count++;
-            }
-            sum += arr[i];
-            System.out.println("Sum is: "+sum+" i: "+i);
-            if(sum == k){
-                count++;
-                sum = 0;
-                i--;
-            }
+            preSum += arr[i];
+
+            int remove = preSum - k;
+
+            count += map.getOrDefault(remove,0);
+
+            map.put(preSum,map.getOrDefault(preSum,0) + 1);
         }
+
         return count;
     }
     public static void main(String[] args) {
