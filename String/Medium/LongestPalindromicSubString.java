5548f04a093359af634f74d62fb69b58c07e5658
MonishGiri <monishgiri46@gmail.com>
2025-04-20 12:44:54 +0530

    Refactor longestPalindrome method to use center expansion approach

--- String/Medium/LongestPalindromicSubString.java
+++ String/Medium/LongestPalindromicSubString.java
@@ -1,45 +1,36 @@
-class LongestPalindromicSubString{
-
-    public static String longestPalindrome(String s) {
-        String result = "";
-        if(s.length() == 1 ) return s;
-        
-        // brute force approach: O(n2)
-        // for(int i=0; i<s.length(); i++){
-        //     for (int j = i+1; j < s.length()-1; j++) {
-        //         if(checkPalindrome(s, i, j)){
-        //             int length = j - i + 1;
-        //             if(length > result.length()){
-        //                 for(int k=i; k<=j; k++){
-        //                     result += s.charAt(k);
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-        int n = s.length();
-        int mid1 = n % 2 == 0 ? (n / 2) -1 : n / 2;
-        int mid2 = n / 2;
-
-        while(mid1<=mid2 && mid1>=0 && mid2<n){
-            if(checkPalindrome(s, mid1, mid2)){
-                result = s.substring(mid1, mid2+1);
-            }
-            mid1--;
-            mid2++;
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
         }
+        
+        String result = "";
+        
+        for (int i = 0; i < s.length(); i++) {
+            // For odd-length palindromes
+            String oddPalindrome = expandFromCenter(s, i, i);
+            if (oddPalindrome.length() > result.length()) {
+                result = oddPalindrome;
+            }
+            
+            // For even-length palindromes
+            String evenPalindrome = expandFromCenter(s, i, i + 1);
+            if (evenPalindrome.length() > result.length()) {
+                result = evenPalindrome;
+            }
+        }
+        
         return result;
     }
 
-    public static boolean checkPalindrome(String s, int low, int high){
-        while(low<high){
-            if(s.charAt(low) != s.charAt(high)) return false;
-            low++;
-            high--;
+    // Helper function to expand the palindrome from the center
+    private String expandFromCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
         }
-        return true;
-    }
-    public static void main(String[] args){
-        System.out.println(longestPalindrome("baba"));
+        
+        // The palindrome is between indices left + 1 and right - 1
+        return s.substring(left + 1, right);
     }
 }
